2a805825656767bbe00efb54b8dd738ec75666d4
Vish75-c <vishgola191@gmail.com>
2026-02-01 22:32:46 +0530

    BackendSetup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Auth from './pages/Auth'
+import LandingPage from './pages/LandingPage'
+import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={Auth}/>
+      <Route path='/' element={LandingPage}/>
+      <Route path='/auth' element={Auth}/>
+      <Route path='/profile' element={Profile}/>
+      <Route path='/dashboard' element={Dashboard}/>
+
+      <Route path='*' element={Auth}/> 
     </Routes>
     </BrowserRouter>
   )
