0ead2054f185286f4bc124ec2c3cab27b3d77dde
Vish75-c <vishgola191@gmail.com>
2026-02-03 01:29:47 +0530

    UserModelCreated

---
+++ Backend/Models/UserModel.js
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    },
+    firstName: {
+        type: String,
+        required: false
+    },
+    lastName: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    color: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    profileSetup: {
+        type: Boolean,
+        default: false
+    },
+    // Role & Academic Logic
+    role:{
+        type:String,
+        enum:["junior","senior","alunni"],
+        default:"junior"
+    },
+    branch:{
+        type:String,
+        required:false
+    },
+    batch:{
+        type:Number,
+        required:false  //Graduation Year
+    },
+    // Professional data (Seniors/alumni)
+    company:{
+        type:String,
+        required:false 
+    },
+    skills:{
+        type:[String],
+        default:[],
+    },
+    // Social & bio
+    bio:{
+        type:String,
+        maxlength:250,
+        required:false
+    },
+    linkedinUrl:{
+        type:String,
+        required:false,
+    },
+    // Metadata 
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+userSchema.pre('save',async function(next){
+    const person=this;
+    if(!person.isModified(password))return next();
+    const salt=await bcrypt.genSalt(10);
+    const hashedPassword=await bcrypt.hash(person.password,salt);
+    person.password=hashedPassword;
+    next();
+})
+
+const UserModel=mongoose.model("Users",userSchema);
+export default User;
