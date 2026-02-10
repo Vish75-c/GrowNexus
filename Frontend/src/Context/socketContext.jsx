883f5bfcc9f7054ed3e658de0923e0232bf0e620
Vish75-c <vishgola191@gmail.com>
2026-02-10 19:34:33 +0530

    SocketBackendAndSocketFrontendSetUpCompletedAlongWithMessageModel

---
+++ Frontend/src/Context/socketContext.jsx
@@ -0,0 +1,49 @@
+import { useAppStore } from "@/store";
+import { HOST } from "@/utils/Constant";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+export const useSocket = () => useContext(SocketContext);
+
+export const SocketProvider = ({ children }) => {
+  const socketRef = useRef(null);
+  const [socket, setSocket] = useState(null);
+  const { userInfo, selectedChatData, selectedChatType } = useAppStore();
+
+  useEffect(() => {
+    if (!userInfo) return;
+    const s = io(HOST, {
+      withCredentials: true,
+      query: { userId: userInfo._id },
+    });
+
+    socketRef.current = s;
+    setSocket(s);
+    s.on("connect", () => {
+      console.log("Connected to Socket Server");
+    });
+
+    const handleReceiveMessage = (message) => {
+      if (
+        selectedChatType &&
+        selectedChatData &&
+        (selectedChatData._id === message.sender._id ||
+          selectedChatData._id === message.recipient._id)
+      ) {
+        console.log(message);
+      }
+    };
+    s.on("recieveMessage", handleReceiveMessage);
+    return () => {
+      s.off("receiveMessage", handleReceiveMessage);
+      s.disconnect();
+      socket.current = null;
+      setSocket(null);
+    };
+  }, [userInfo]);
+
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
+import { SocketProvider } from './Context/socketContext'
 createRoot(document.getElementById('root')).render(
-  <>
+  <SocketProvider>
     <App />
     <Toaster closeButton/>
-  </>,
+  </SocketProvider>,
 )
